f62bc05b508b56e766ca8b85cc23bfe0dc040518
Rohit Bhakuni <rohitbhakuni65@gmail.com>
2025-07-16 16:13:10 +0530

    feat: temporarily hide projects to test deployment

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
           {/* <Tech /> */}
           <Works />
           <Feedbacks />
-          {/* <Contact /> */}
+          <Contact />
         </div>
       </div>
     </BrowserRouter>
